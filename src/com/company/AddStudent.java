9bdcd45441eab2b66dd0131969b991a7a887b67a
aemreb <ahmetemreboyaci@gmail.com>
2020-12-31 17:55:39 +0300

    UI eklendi

---
+++ src/com/company/AddStudent.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import javax.swing.*;
+
+public class AddStudent {
+
+    public JPanel panelMain;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JButton gönderButton;
+
+
+    public static void main(String[] args){
+
+    }
+}
