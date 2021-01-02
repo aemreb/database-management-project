76ed3aad061cbaa012468abfa5818de1232a6ba7
aemreb <ahmetemreboyaci@gmail.com>
2021-01-02 13:46:25 +0300

    UI eklendi

--- src/StudentInfo.java
+++ src/StudentInfo.java
@@ -1,2 +1,51 @@
-public class StudentInfo {
+import com.company.AddStudent;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StudentInfo extends JFrame{
+    private JLabel isimTitle;
+    private JLabel soyisimTitle;
+    private JLabel tcTitle;
+    private JLabel yasTitle;
+    private JLabel telefonTitle;
+    private JLabel emergencyTitle;
+    private JLabel emailTitle;
+    private JLabel adresTitle;
+    private JLabel isimValue;
+    private JLabel soyisimValue;
+    private JLabel tcValue;
+    private JLabel yasValue;
+    private JLabel telefonValue;
+    private JLabel emergencyValue;
+    private JLabel emailValue;
+    private JLabel adresValue;
+    private JPanel studentInfoPanel;
+    private JTextField enterStudentInfoTextField;
+    private JButton searchButton;
+    private JFrame frame= new JFrame();
+    private String studentInfo;
+
+
+    public StudentInfo() {
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                studentInfo = enterStudentInfoTextField.getText();
+            }
+        });
+    }
+
+    public void load(){
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.getContentPane().add(new StudentInfo().studentInfoPanel);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
 }
+
+
+
--- src/com/company/AddStudent.java
+++ src/com/company/AddStudent.java
@@ -1,22 +1,59 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 
-public class AddStudent {
+public class AddStudent extends JFrame {
 
-    public JPanel panelMain;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTextField textField7;
-    private JTextField textField8;
-    private JButton gönderButton;
+    private JPanel panelMain;
+    private JTextField isimField;
+    private JTextField soyisimField;
+    private JTextField tcField;
+    private JTextField yasField;
+    private JTextField telefonField;
+    private JTextField emergencyField;
+    private JTextField emailField;
+    private JTextField adresField;
+    private JLabel isimTitle;
+    private JLabel soyisimTitle;
+    private JLabel tcTitle;
+    private JLabel yasTitle;
+    private JLabel telefonTitle;
+    private JLabel emergencyTitle;
+    private JLabel emailTitle;
+    private JLabel adresTitle;
+    private JButton gonderButton;
+    public JFrame frame = new JFrame();
+    public AddStudent(){
 
+        gonderButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button pressed");
+                frame.setVisible(false);
+                frame.dispose();
+                //???
 
-    public static void main(String[] args){
+            }
+        });
+    }
+
+    private void close(){
+        WindowEvent windowEventClosing = new WindowEvent(this, WindowEvent.WINDOW_CLOSING);
+        Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(windowEventClosing);
+    }
+
+    public void load(){
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.getContentPane().add(new AddStudent().panelMain);
+        frame.pack();
+        frame.setVisible(true);
+
 
     }
+
+
 }
